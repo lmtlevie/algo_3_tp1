963589ca566ab673ce2d60c0c7fc81c4a8afa4df
Luciano Levy <86862363+lmtlevie@users.noreply.github.com>
2023-04-12 20:41:28 -0300

    orden lexicografico e ideas

--- ej1_backtracking.cpp
+++ ej1_backtracking.cpp
@@ -1,24 +1,29 @@
+#include <vector>
+#include <cmath>
+#include <iostream>
 
-int magico = (pow(n,3) + n)/2);
+#include <numeric>
+using namespace std;
+
+int magico;
 // TENER UNA FUNCION QUE TE DEVUELVE EL ORDEN LEX MINIMO QUE SE
 // PUEDE ARMAR CON LA MATRIZ PARCIAL SI RELLENAMOS LOS ESPACIOS
 // SIGUIENTE CON LAS VALORES MINIMOS (combinatoria)
 
 vector<vector<int>> k_cuadrado_magico(int k,vector<int> nums,vector<vector<int>> parcial,int i,int j){
 
-    if (nums.empty() && orden_minimo_parcial_lex(parcial) == k){
+    if (nums.empty()){
         return parcial;
     }
 
-    parcial[i][j] = nums.back;
-    nums_back.pop_back();
+    // saco del inicio para generar los cuadrados lexicograficamentes ascendentes
+    parcial[i][j] = nums.front();
+    nums.erase(nums.begin());
 
 
-    if( (fila_parcial(parcial) < magico && columna_parcial(parcial) < magico) &&
-        // te fijas si las columnas o filas completas dan el magico
-        (c_magico_parcial_valido(parcial)) &&
-            orden_minimo_parcial_lex(parcial) < k
-        )
+    if((fila_parcial(parcial) < magico && columna_parcial(parcial) < magico))
+        // te fijas si las columnas o filas completas dan el magico si i == n o j == n
+        if ((c_magico_parcial_valido(parcial)))
 
     {
 
@@ -28,13 +33,26 @@ vector<vector<int>> k_cuadrado_magico(int k,vector<int> nums,vector<vector<int>>
 }
 
 
-void cuadrado_final(){
-
+void cuadrado_final(vector<vector<int>>){
+    return;
 }
 
 void main(){
-   vector<int> numeros[n];
-   vector<vector<int>> vacio[n][n];
+   int n;cin >> n;
+   int k;cin >> k;
+   
+   // numeros ordenoas de 1 a n cuadrado
+   vector<int> numeros(pow(n,2));
+   iota(begin(numeros),end(numeros),1);
+   
+   //magico
+   magico = (pow(n,3) + n); // matriz vacia
+   magico = magico/2;
+   
+   //matriz vacia
+   vector<vector<int>> vacio(n,vector<int> (n));
+   
+   //llamada
    vector<vector<int>> resultado = k_cuadrado_magico(k,numeros,vacio,0,0);
 
    cuadrado_final(resultado);
