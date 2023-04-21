a99c142d0cf5f57b14bc4ef6b42941edf3850b0d
Luciano Levy <86862363+lmtlevie@users.noreply.github.com>
2023-04-21 19:31:11 -0300

    Ejercio aceptado por el juez!!

--- ej_2_lucho.cpp
+++ ej_2_lucho.cpp
@@ -6,22 +6,6 @@ using namespace std;
 #include <numeric>
 
 
-
-int64_t mod_bin_exp(int64_t x, int64_t y, int64_t m) {
-
-    int64_t res = 1;
-    while (y > 0) {
-        if (y % 2 == 1) {
-            res = (res * x) % m;
-        }
-        x = (x * x) % m;
-        y = y / 2;
-    }
-    return res;
-    
-}
-
-
 int64_t modulo(int64_t val, int64_t m){
     
     if( val >= 0 ){
@@ -35,56 +19,62 @@ int64_t modulo(int64_t val, int64_t m){
     
     return res;
 }
-
-
-bool f(vector<int64_t>& v , vector<vector<vector<int64_t>>>& memo , int64_t m , int64_t r, int64_t acumulado , int64_t i){
-    if( i == v.size() ){
-        return (acumulado == r);
-        
+int64_t mod_bin_exp(int64_t x, int64_t y, int64_t m) {
+   /* if(y==0){
+        return 1;
     }
     
-
-    //suma
-    int64_t suma;
-    if (memo[0][i][acumulado] != -1){
-        suma = memo[0][i][acumulado];
+    int64_t value = mod_bin_exp(x,y/2,m);
+    int64_t res = (int64_t)pow(value,2) % m;
+    if(y % 2 == 0){
+        return res % m;
     }else{
-        suma = (acumulado + v[i]) % m;
-        memo[0][i][acumulado] = suma;
+        return (res*x)%m;
     }
-    //mult
-    int64_t mult;
-    if (memo[1][i][acumulado] != -1){
-        mult = memo[1][i][acumulado];
-    }else{
-        mult = (acumulado * v[i]) % m;
-        memo[1][i][acumulado] = mult;
+    */
+    
+    int64_t res = 1;
+    while (y > 0) {
+        if (y % 2 == 1) {
+            res = modulo((res * x), m);
+        }
+        x = modulo((x * x), m);
+        y = y / 2;
     }
-    //pot
-    int64_t pot;
-    if (memo[2][i][acumulado] != -1){
-        pot = memo[2][i][acumulado];
-    }else{
-        pot = mod_bin_exp(acumulado,v[i], m);
-        memo[2][i][acumulado] = pot;
-    }
-    //res
-    int64_t res;
-    if (memo[3][i][acumulado] != -1){
-        res = memo[3][i][acumulado];
-        memo[3][i][acumulado] = res;
-    }else{
-        res = modulo((acumulado - v[i]),m); 
-    }
-
-    //memo[i][acumulado] = memo[i][(acumulado + v[i]) % m]  +  memo[i][(int64_t) ((int64_t64_t) acumulado * (int64_t64_t)v[i]) % m]  + memo[i][ mod_bin_exp(acumulado,v[i], m)]    + 
-    return f(v,memo,m,r, suma , i+1) || f(v,memo,m,r, mult , i+1) || f(v,memo,m,r, pot , i+1) || f(v,memo,m,r, res , i+1);
-;
-;
-;
-;
+    return res;
+    
 }
 
+
+
+
+
+bool f(vector<int64_t>& v , vector<vector<bool>>& memo , int64_t m , int64_t r, int64_t acumulado , int i){
+    if( i == v.size() ){
+        int64_t a = modulo(acumulado,m);
+        return (a == r);
+        
+    }
+    bool res = false;
+    int64_t valor = modulo(v[i],m);
+
+    if(memo[i][acumulado]){
+        
+        int64_t suma = modulo((acumulado + valor),m);
+        
+        int64_t mult = modulo((acumulado * valor),m);
+        
+        int64_t pot = mod_bin_exp(acumulado,valor, m);
+
+        int64_t res = modulo((acumulado - valor),m);
+    
+        res =  f(v,memo,m,r, suma , i+1) || f(v,memo,m,r, mult , i+1) || f(v,memo,m,r, pot , i+1) || f(v,memo,m,r, res , i+1);
+        memo[i][acumulado] = res;
+        return res;
+    }
+    
+    return res;
+}
 
 
 int main()
@@ -94,15 +84,19 @@ int main()
        int64_t n;cin >> n;
        int64_t r;cin >> r;
        int64_t m;cin >> m;
-     
+       vector<vector<bool>> memo(n, vector<bool>(m,true));
        vector<int64_t> v;
-       vector<vector<vector<int64_t>>> memo(4, vector<vector<int64_t>>(n, vector<int64_t>(m-1, -1)));
        while( n-- ){
            int64_t val;
            cin >> val;
            v.push_back(val);
        }            
-       cout << f(v,memo,m,r,v[0],1);
+       bool res = f(v,memo,m,r,v[0],1);
+       if (res){
+            cout << "Si" << endl;
+       }else{
+            cout << "No" << endl;
+       }
 
    }
     return 0; 
